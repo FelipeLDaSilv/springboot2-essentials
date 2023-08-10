8ea9ae2b7676654a77deb07873400abc080d1b94
FelipeLDaSilv <felipejesusamado@gmail.com>
2023-08-10 13:24:44 -0300

    first commit

---
+++ src/main/java/academy/controller/AnimeController.java
@@ -0,0 +1,22 @@
+package academy.controller;
+
+import academy.domain.Anime;
+import academy.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping(path = "/anime")
+public class AnimeController {
+    @Autowired
+    private DateUtil dateUtil;
+    @GetMapping(path = "/list")
+    public List<Anime> list(){
+        return List.of(new Anime("DBZ"), new Anime("Berserk"));
+    }
+
+
+}
---
+++ src/main/java/academy/util/DateUtil.java
@@ -0,0 +1,12 @@
+package academy.util;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+@Component
+public class DateUtil {
+    public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime){
+        return DateTimeFormatter.ofPattern("yyyy-MM-ddHH:mm:ss").format(localDateTime);           //
+    }
+}
